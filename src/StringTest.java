67587ffd188d50823c6cdadebffdee01ae238726
Arka Raha <arka.raha2000@gmail.com>
2023-11-14 17:51:11 +0530

    New Check

---
+++ src/StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest {
+    public static void main(String[] args) {
+     String str = "arka";
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(2,3));
+    }
+}
